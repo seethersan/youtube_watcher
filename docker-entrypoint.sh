79f71c3ccda4011195a907a7828fec8368687a68
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-02-07 00:23:33 -0500

    add migrate

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@
 service cron restart
 python /code/manage.py runcrons
 # Django
+python /code/manage.py migrate
 python /code/manage.py collectstatic --noinput
 python /code/manage.py runserver 0.0.0.0:8000
 exec "$@"
